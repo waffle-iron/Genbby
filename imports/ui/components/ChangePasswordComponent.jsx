f760f41f42418995d8b3cdcc59d7b532e2d0561d
Diego <dg.cabanillas@uni.pe>
2017-03-03 10:25:43 -0500

    reco and change styles

--- imports/ui/components/ChangePasswordComponent.jsx
+++ imports/ui/components/ChangePasswordComponent.jsx
@@ -4,38 +4,44 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
  
 export default class ChangePasswordComponent extends Component{
-		onChange(e) {
-			e.preventDefault();
-			const oldPassword = ReactDOM.findDOMNode(this.refs.password).value.trim();
-			const newPassword = ReactDOM.findDOMNode(this.refs.newpassword).value.trim();
-			const newPasswordConfirm = ReactDOM.findDOMNode(this.refs.newpasswordconfirm).value.trim();
-			if(newPassword==newPasswordConfirm)	
-					Accounts.changePassword( oldPassword , newPassword, (error)=>{ 
-						if(!error)
-							Materialize.toast(er.reason, 4000);
-						else
-							FlowRouter.go('/home');
-						} );
-
+	onChange(e) {
+		e.preventDefault();
+		const oldPassword = ReactDOM.findDOMNode(this.refs.password).value.trim();
+		const newPassword = ReactDOM.findDOMNode(this.refs.newpassword).value.trim();
+		const newPasswordConfirm = ReactDOM.findDOMNode(this.refs.newpasswordconfirm).value.trim();
+		if(newPassword==newPasswordConfirm)	{
+			Accounts.changePassword( oldPassword , newPassword, (error)=>{ 
+				if(!error)
+					Materialize.toast(er.reason, 4000);
+				else
+					FlowRouter.go('/home');
+			});
 		}
-		render(){
-			return(
-					<div className="container">
-						<header>
-							<h1>Change Pasword</h1>
-							<form onSubmit={this.onChange.bind(this)} >
-								<ul>Type Current Pasword<input type="password" ref="password" placeholder="Current Password" required
-								/></ul>
-								<ul>Type New Password<input type="password" ref="newpassword" placeholder="New Password" required
-								/></ul> 
-								<ul>Confirm New Password<input type="password" ref="newpasswordconfirm" placeholder="Confirm Password" required
-								/></ul> 
-								<ul>	
-									<button type="submit" className="col s4 waves-effect waves-light btn">change</button>
-								</ul>
-							</form>
-						</header>
-					</div>
-					);
-		}
+	}
+	render(){
+		return(
+			<div className="row">
+				<div className="col s12 m6 offset-m3 l4 offset-l4">
+					<h3 className="center-align">Change Password</h3>
+					<form onSubmit={this.onChange.bind(this)} className="col s12">
+						<div className="input-field">
+							<input type="password" id="changePassword" ref="password"/>
+							<label htmlFor="changePassword">Type Current Pasword</label>
+						</div>
+						<div className="input-field">
+							<input type="password" id="changeNewPassword" ref="newpassword"/>
+							<label htmlFor="changeNewPassword">Type New Password</label>
+						</div>
+						<div className="input-field">
+							<input type="password" id="changeNewPasswordConfirm" ref="newpasswordconfirm"/>
+							<label htmlFor="changeNewPassword">>Confirm New Password</label>
+						</div>
+						<div className="col s10 offset-s1">
+							<button type="submit" className="col s12 waves-effect waves-light btn">change</button>
+						</div>
+					</form>
+				</div>
+			</div>	
+		);
+	}
 }
